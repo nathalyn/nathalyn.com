0e066119376b9e8c6c9091c9134e61bf0c69a852
nathalyn <nathalyn.nunoo@gmail.com>
2017-12-31 23:47:35 -0500

    Fixed up media queries

--- scripts.js
+++ scripts.js
@@ -17,16 +17,16 @@
 //
 
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('#myBtn').fadeIn();
-        } else {
-            $('#myBtn').fadeOut();
-        }
-    });
-    $('#myBtn').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 600);
-        return false;
-    });
-});
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if ($(this).scrollTop() > 100) {
+//             $('#myBtn').fadeIn();
+//         } else {
+//             $('#myBtn').fadeOut();
+//         }
+//     });
+//     $('#myBtn').click(function(){
+//         $("html, body").animate({ scrollTop: 0 }, 600);
+//         return false;
+//     });
+// });
